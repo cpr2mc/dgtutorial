845e1b50cec0879c147943e1426e0dbb1f960b1f
Chris <roos.chris.p@gmail.com>
2025-05-05 13:13:21 -0400

    added blocks around polls app, added voting and vote tracking

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('polls', include('poll.urls')),
+    path('polls/', include('poll.urls')),
 ]
